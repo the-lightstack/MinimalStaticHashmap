f4408efa7a5d8277555cfe087bc43b941944a2f9
bottleInALightning <lightningbottle@protonmail.com>
2025-06-11 18:04:14 +0200

    intermediate version, (not working)

--- src/hmap.rs
+++ src/hmap.rs
@@ -1,5 +1,5 @@
-// Will provide load of 96%
-const ENTRIES:u64 = 4194304;
+// Will provide load of 80%, resulting in 2 gb of memory being used
+const ENTRIES:u64 = 5033164;
 const DIMENSIONS: u64 = 100;
 
 
@@ -21,24 +21,28 @@ pub const fn fnv_hash(bytes: &[u8]) -> u64 {
 }
 
 
-
 // struct EmbeddingEntry{
 //     vectors: [f32; DIMENSIONS],
 // }
 
-struct PlaceHolderEntry{
-    is_full: bool,
+#[derive(Clone, Copy,Debug)]
+pub struct PlaceHolderEntry{
+    pub is_full: bool,
 }
-
 
 // Will currently take up 1.61 GB of memory (of raw vector-data)
 pub struct MinimalHashMap{
-    entries: [PlaceHolderEntry;ENTRIES as usize],
+    entries: Box<[PlaceHolderEntry;ENTRIES as usize]>,
     collisions_counter: u32
 }
 
 impl MinimalHashMap{
-    fn insert(&mut self, word: &str){
+    pub fn new() -> Self{
+        let placeholder = PlaceHolderEntry{is_full:false};
+        Self { entries: Box::new([placeholder;ENTRIES as usize]), collisions_counter: 0 }
+    }
+
+    pub fn insert(&mut self, word: &str){
         let h = fnv_hash(&word.as_bytes());
         let ind = h % ENTRIES;
         // Check if
@@ -47,7 +51,47 @@ impl MinimalHashMap{
         }else{
             self.entries[ind as usize].is_full = true;
         }
+    }
 
+    fn score_spread(&self)->f64{
+        // Clusters give big minus points, otherwise we ideally want equal spacing between the holes
+        let ideal_spacing = (ENTRIES as f64) / (self.collisions_counter as f64);
+
+        // Bigger spaces should be weighed heavier, so let's do quadratic (like variance)
+        let mut score:f64 = 0.0;
+        let mut hole_chunks:usize = 0;
+        let mut last_hole_pos = 0;
+
+
+        let v = self.entries.as_slice();
+
+        for (pos,entry) in v.iter().enumerate(){
+            if !entry.is_full{
+                if last_hole_pos+1 == pos{
+                    hole_chunks += 1;
+                }
+                let dist = pos - last_hole_pos;
+                score += (dist * dist) as f64;
+                last_hole_pos = pos;
+            }
+        };
+
+
+
+        let default_spacing_variance = score/(self.collisions_counter as f64);
+
+        let final_score = (default_spacing_variance - ideal_spacing).powi(3);
+        final_score
+
+    }
+
+    pub fn info(&self){
+        let col_perc = self.collisions_counter as f64/(ENTRIES as f64)*100.0;
+        println!("Collisions: {} (which is {:.6}% of total)",self.collisions_counter,col_perc);
+
+        let score_spread = self.score_spread();
+        println!("Spread-score: {:.5}",score_spread);
+        // I want also want to know how spread apart the "holes" are because better distribution => better for linear probing
 
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::collections::HashMap;
-use std::hash::RandomState;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
-use std::io::BufWriter;
-use std::mem::size_of;
+use std::thread;
+use std::time::Duration;
+use std::vec;
+// use std::mem::size_of;
+
+use crate::hmap::MinimalHashMap;
+use crate::hmap::PlaceHolderEntry;
 
 mod hmap;
 
-#[repr(C,packed(1))]
-struct Embedding{
-    values: [f32;100], // probably way too precise, don't actually need mantissa and all that, just a sign bit and the rest can be "like" u8/u16 -> TODO
-}
+// struct Embedding{
+//     values: [f32;100], // probably way too precise, don't actually need mantissa and all that, just a sign bit and the rest can be "like" u8/u16 -> TODO
+// }
 
 // We will then just scale them all into i16 for less mantissa and stuff wasted
 // impl From<Vec<f32>> for Embedding{
@@ -25,48 +27,48 @@ struct Embedding{
 //     }
 // }
 
-#[repr(C)]
 struct Parser{
-    map: HashMap<String,Embedding>,
+    map: MinimalHashMap,
 }
 
-#[cfg(test)]
-mod tests {
-    // TODO: we can surely improve this with SIMD and other processor specific operations, have supervise compiler here
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     // TODO: we can surely improve this with SIMD and other processor specific operations, have supervise compiler here
+//     use super::*;
 
-    #[test]
-    fn test_dot_product() {
-        let parser = Parser{map:HashMap::new()};
+//     #[test]
+//     fn test_dot_product() {
+//         let parser = Parser{map:HashMap::new()};
 
-        let v1:Vec<f32> = vec![-1.0,2.1,-2.3];
-        let v2:Vec<f32> = vec![3.0,1.9,-1.2];
+//         let v1:Vec<f32> = vec![-1.0,2.1,-2.3];
+//         let v2:Vec<f32> = vec![3.0,1.9,-1.2];
 
-        let res = parser.dot_product(&v1, &v2);
-        assert_eq!(res,3.749999761581421);
-    }
+//         let res = parser.dot_product(&v1, &v2);
+//         assert_eq!(res,3.749999761581421);
+//     }
 
-    #[test]
-    fn test_vec_mag() {
-        let parser = Parser{map:HashMap::new()};
+//     #[test]
+//     fn test_vec_mag() {
+//         let parser = Parser{map:HashMap::new()};
 
-        let v1:Vec<f32> = vec![1.0,2.0,3.0,-5.0];
+//         let v1:Vec<f32> = vec![1.0,2.0,3.0,-5.0];
 
-        let res = parser.vec_mag(&v1);
-        assert_eq!(res,6.244997998398398);
-    }
+//         let res = parser.vec_mag(&v1);
+//         assert_eq!(res,6.244997998398398);
+//     }
 
-}
+// }
 
 struct WordNotFoundError{}
 
 
 impl Parser{
-    fn init()->Self{
+    pub fn new(hm: MinimalHashMap)->Self{
         // IMPORTANT: the capacity is a match for the ENGLISH words list (2**22 > 4027169)
         // IDEA: we could loop over seeds to find one that has the least collisions and use that -- don't really have to worry about hashDOS
-        Self { map: HashMap::with_capacity_and_hasher(4194304, RandomState::new())}
+        // Self { map: HashMap::with_capacity_and_hasher(4194304, RandomState::new())}
 
+        Self{map: hm}
     }
 
 
@@ -75,17 +77,17 @@ impl Parser{
         let line_segments = line.split_once(" ").unwrap();
         let word = line_segments.0;
 
-        let values = line_segments.1.split(" ").map(|x|{
-            let i:f32 = x.parse().expect("Found corrupt value in file");
-            i
-        }).into_iter();
+        // let values = line_segments.1.split(" ").map(|x|{
+        //     let i:f32 = x.parse().expect("Found corrupt value in file");
+        //     i
+        // }).into_iter();
 
-        let mut emb = Embedding{values:[0.0;100]};
-        for (pos,v) in values.enumerate(){
-            emb.values[pos] = v;
-        }
+        // let mut emb = Embedding{values:[0.0;100]};
+        // for (pos,v) in values.enumerate(){
+        //     emb.values[pos] = v;
+        // }
 
-        self.map.insert(word.to_string(), emb);
+        self.map.insert(&word);
     }
 
 
@@ -100,75 +102,95 @@ impl Parser{
 
     }
 
-    fn check_similarity(&self,word_1:&str, word_2:&str)->Result<f64,WordNotFoundError>{
-        let v1 = self.map.get(word_1).ok_or(WordNotFoundError{})?.values;
-        let v2 = self.map.get(word_2).ok_or(WordNotFoundError{})?.values;
+    // fn check_similarity(&self,word_1:&str, word_2:&str)->Result<f64,WordNotFoundError>{
+    //     let v1 = self.map.get(word_1).ok_or(WordNotFoundError{})?.values;
+    //     let v2 = self.map.get(word_2).ok_or(WordNotFoundError{})?.values;
 
 
-        let sim = (
-            self.dot_product(&v1, &v2)
-        )/(
-            self.vec_mag(&v1) * self.vec_mag(&v2)
-        );
-        Ok(sim)
-    }
+    //     let sim = (
+    //         self.dot_product(&v1, &v2)
+    //     )/(
+    //         self.vec_mag(&v1) * self.vec_mag(&v2)
+    //     );
+    //     Ok(sim)
+    // }
 
-    fn print_sim(&self,word_1:&str, word_2:&str){
-        let res = self.check_similarity(word_1, word_2);
-        match res {
-            Ok(r) =>{
-                println!("{} -> {}: {}",word_1,word_2,r)
-            },
-            Err(_)=>{
-                println!("Couldn't find {} or {}",word_1,word_2)
-            }
-        }
+    // fn print_sim(&self,word_1:&str, word_2:&str){
+    //     let res = self.check_similarity(word_1, word_2);
+    //     match res {
+    //         Ok(r) =>{
+    //             println!("{} -> {}: {}",word_1,word_2,r)
+    //         },
+    //         Err(_)=>{
+    //             println!("Couldn't find {} or {}",word_1,word_2)
+    //         }
+    //     }
 
-    }
+    // }
 }
 
 
 //
 
 fn main() {
+    let ph = PlaceHolderEntry{is_full:false};
+    let c = [ph;10];
+
+    println!("C entries: {:?}",c[1]);
+
+
+
     let f = File::open("./English/en.vectors").expect("Download word2vec vectors under ./English/en.vectors");
     let mut reader = BufReader::new(f);
 
-    let mut parser = Parser{
-        map: HashMap::new()
-    };
+    let hm = MinimalHashMap::new();
+    let mut parser = Parser::new(hm);
 
     // TODO: make this max line length
     let mut line = String::new();
 
-
+    let mut words:Vec<String> = vec![];
 
     let mut skipped_header = false;
     loop{
-        if let Ok(_len) = reader.read_line(&mut line){
+        match  reader.read_line(&mut line){
+            Ok(_len) =>{
             // EOF
-            if _len == 0{continue;}
-
+            if _len == 0{break;}
 
             // if line is still empty, asssume we are handling the first one and just skip
             if !skipped_header {skipped_header = true; line = String::from(""); continue}
 
-            parser.insert_line(&line.trim());
-            // let res = parser.parse_line(&line.trim());
-            // parser.add_embedding(res);
-            line = String::from("")
+            let word = &line.trim().split_once(" ").unwrap().0;
 
-        }else { break;}
+            words.push(word.to_string());
+            // parser.insert_line(&line.trim());
+            // // let res = parser.parse_line(&line.trim());
+            // // parser.add_embedding(res);
+            line = String::from("");
+        },
+        Err(_)=>{}
+    }}
+
+    // Testing seeds for least collisions and least chunks
+    loop{
+        
+
     }
+
+    // parser.map.info();
     println!("[!] All vecs loaded into memory");
-    println!("[...] Writing Parser struct to binary file...");
+
+    thread::sleep(Duration::from_secs(10));
+
+    // println!("[...] Writing Parser struct to binary file...");
 
     // let f = File::open("./vectors.bin").expect("Want to write to binary file...");
     // let writer = BufWriter::new(f);
 
 
-    let s = size_of::<Parser>();
-    println!("Size of parser: {}",s);
+    // let s = size_of::<Parser>();
+    // println!("Size of parser: {}",s);
 
     // parser.print_sim("computer", "machine");
     // parser.print_sim("man", "woman");
